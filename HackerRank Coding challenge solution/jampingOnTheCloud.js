cbb3ab4fb4394afc3243a19334b0cc59b3daf2ef
Birhan Abuhay Jemere <128576443+Bura-Abuh@users.noreply.github.com>
2023-04-04 02:04:45 +0300

    jumping to the cloud games
    jumping to the cloud games

---
+++ HackerRank Coding challenge solution/jampingOnTheCloud.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(c) {
+    // Write your code here
+ 
+    let jumps = 0;
+    let i = 0;
+    while (i < c.length - 1) {
+        if (c[i + 2] === 0) {
+            i += 2;
+        } else {
+            i += 1;
+        }
+        jumps += 1;
+    }
+    return jumps;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const result = jumpingOnClouds(c);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
